67198563679a0fc0b07268fc7522bdc99722df0b
Kamil Shaikha <95491864+KamilSh98@users.noreply.github.com>
2022-07-11 14:50:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+createButton = document.getElementById("create");
+passwordBox = document.getElementById("passwordBox");
+
+createButton.addEventListener("click", () => {
+    
+    passwordBox.innerHTML = "Essa";
+})
